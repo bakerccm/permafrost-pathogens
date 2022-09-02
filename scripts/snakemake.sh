70d5ac16f6a775b93f8f7654c707261c22d13ef3
Christopher Baker <cbaker@login4.frontera.tacc.utexas.edu>
2022-09-02 10:59:51 -0500

    update snakemake.sh

--- scripts/snakemake.sh
+++ scripts/snakemake.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH -N 1  # nodes
 #SBATCH -n 56  # cores (note TACC allocates whole nodes)
-#SBATCH -t 0-06:00  # runtime in D-HH:MM
+#SBATCH -t 0-04:00  # runtime in D-HH:MM
 #SBATCH -p small  # partition to submit to
 ##SBATCH --mem=192G  # total mem (note TACC allocates whole nodes; using this argument causes job to fail)
-#SBATCH -J megahit_NT
-#SBATCH -o slurm/megahit_NT.out
-#SBATCH -e slurm/megahit_NT.err
+#SBATCH -J snakemake
+#SBATCH -o slurm/snakemake.out
+#SBATCH -e slurm/snakemake.err
 ##SBATCH --mail-type=BEGIN,END    # notifications: BEGIN,END,FAIL,ALL
 ##SBATCH --mail-user=
-#SBATCH --dependency=afterok:4215851 # job can begin after specified jobs have completed with exit code zero
 
 ##SBATCH --dependency=after:jobid[:jobid...] # job can begin after specified jobs have started
 ##SBATCH --dependency=afterany:jobid[:jobid...] # job can begin after specified jobs have terminated
@@ -22,13 +21,13 @@
 
 source activate snakemake
 
-# snakemake -j 56 --use-conda sickle_multiQC fastq_join_multiQC
+snakemake -j 56 --use-conda all_bowtie2_mapping
 
 # snakemake -j 56 --use-conda out/megahit/35m/final.contigs.fa
 # snakemake -j 56 --use-conda out/megahit/45m/final.contigs.fa
 # snakemake -j 56 --use-conda out/megahit/60m/final.contigs.fa
 # snakemake -j 56 --use-conda out/megahit/83m/final.contigs.fa
-snakemake -j 56 --use-conda out/megahit/NT/final.contigs.fa
+# snakemake -j 56 --use-conda out/megahit/NT/final.contigs.fa
 
 # snakemake -j 8 --use-conda some_rule --forcerun upstream_rule
 
