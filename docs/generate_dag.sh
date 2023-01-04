4d9231601b59cc9f058fda5ae1a6357bbe3ccfca
Chris Baker <bakerccm@gmail.com>
2023-01-03 23:23:58 -0500

    update flag from -j to -c

--- docs/generate_dag.sh
+++ docs/generate_dag.sh
@@ -2,9 +2,9 @@
 
 # the default rule 'all', plus the three rules 'descriptive', 'missingdata' and 'coinertia_etc'
 # (which do not have output files), should be sufficient to re-run the code in its entirety:
-snakemake -j 1 -n -F all
+snakemake -c 1 -n -F all
 
 # generate DAG etc using:
-snakemake -j 1 -n -F all --dag | dot -Tpdf >docs/snakemake_dag.pdf
-snakemake -j 1 -n -F all --rulegraph | dot -Tpdf >docs/snakemake_rulegraph.pdf
-snakemake -j 1 -n -F all --filegraph | dot -Tpdf >docs/snakemake_filegraph.pdf
+snakemake -c 1 -n -F all --dag | dot -Tpdf >docs/snakemake_dag.pdf
+snakemake -c 1 -n -F all --rulegraph | dot -Tpdf >docs/snakemake_rulegraph.pdf
+snakemake -c 1 -n -F all --filegraph | dot -Tpdf >docs/snakemake_filegraph.pdf
