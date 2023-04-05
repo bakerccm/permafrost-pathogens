623bae6859c104574295f02c1e6e8e699542861b
Chris Baker <cbaker@node117.rcchpc>
2023-04-04 22:07:41 -0400

    stash spare code

--- code/singlem_taxa_of_interest.sh
+++ code/singlem_taxa_of_interest.sh
@@ -7,3 +7,6 @@ do
     grep 'Parvibaculum' $file
 done > out/singlem/taxa_of_interest.txt
 
+# spare code
+# grep 'Stenotrophomonas' all_good_samples.otu_table.tsv | awk '{print $1,$2,$4}' >singlem_results_stenotrophomonas.txt
+
