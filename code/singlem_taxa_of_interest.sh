a58118e9219f4fa4a82371fbc4967a5f3fd77110
Chris Baker <bakerccm@gmail.com>
2023-04-12 16:17:30 -0400

    add grep for family

--- code/singlem_taxa_of_interest.sh
+++ code/singlem_taxa_of_interest.sh
@@ -5,8 +5,22 @@ do
     echo $file
     grep 'Stenotrophomonas' $file
     grep 'Parvibaculum' $file
+    echo
 done > out/singlem/taxa_of_interest.txt
 
 # spare code
 # grep 'Stenotrophomonas' all_good_samples.otu_table.tsv | awk '{print $1,$2,$4}' >singlem_results_stenotrophomonas.txt
+
+for file in out/singlem/*.tsv
+do
+    grep 'Xanthomonadaceae$' $file
+    echo
+done > out/singlem/taxa_of_interest_Xanthomonadaceae.txt
+
+for file in out/singlem/*.tsv
+do
+    grep 'Parvibaculaceae$' $file
+    echo
+done > out/singlem/taxa_of_interest_Parvibaculaceae.txt
+
 
